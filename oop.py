71986fde65777ae6b7c57838735d1e5fdca1b62f
hairneeorlah <eniolabals0@gmail.com>
2019-03-21 03:46:48 -0700

    my initial start of repository

---
+++ oop.py
@@ -0,0 +1,30 @@
+# class Person:
+#     mouth = 1
+#     hands = 2
+#     legs  = 2
+    
+#     def walk(self):
+#         print("walk walk walk walk walk walk")
+         
+# class Politician(Person): 
+#     pass
+# new_person = Person()
+
+
+# class Data:
+#     def __init__(self, main_data):
+#         self.main_data = main_data
+#         self._length = len(main_data)
+#         self.unique = str(set(main_data))
+#         self.mean = self.avg()
+
+#     def avg(self):
+#         numerator = sum( self.main_data)
+#         denominator = self._length   
+#         mean = numerator/denominator
+#         return mean
+
+# rand = Data([1,2,3,4,5,6,7,8,9]) 
+# print(rand.mean)
+
+
